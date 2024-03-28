210d4738cd8aaaa475f37d44f03ca76daafb6bae
Ananthu Maheswaran <ananthumaheswaran098@gmail.com>
2024-03-28 23:35:25 +0530

    Moved the contacts route to be a child of the root route

--- src/main.jsx
+++ src/main.jsx
@@ -11,13 +11,14 @@ const router = createBrowserRouter([
     path: "/",
     element: <Root />,
     errorElement: <ErrorPage />,
-  },
-  {
-    path: "contacts/:contactId",
-    element: <Contact />,
+    children: [
+      {
+        path: "contacts/:contactId",
+        element: <Contact />,
+      },
+    ],
   },
 ]);
-
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <RouterProvider router={router} />
